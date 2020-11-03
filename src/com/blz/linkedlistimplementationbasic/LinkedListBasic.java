88e050c8c94fbc0c3af0f537744bdb0d8f000d19
Nisha271 <bukkenisha@gmail.com>
2020-11-03 19:28:01 +0530

    [Archana] Added: UC1 with created simple Linkedlist and add one node

--- src/com/blz/linkedlistimplementationbasic/LinkedListBasic.java
+++ src/com/blz/linkedlistimplementationbasic/LinkedListBasic.java
@@ -1,10 +1,24 @@
 package com.blz.linkedlistimplementationbasic;
 
 public class LinkedListBasic {
-
+	public Node head = null;
+	public Node tail = null;
 	
+	public void addNodeToLinkedList(int data) {
+		Node newNode = new Node(data,null);
+		head = newNode;
+		tail = newNode;
+		
+	}
+	public void display() {
+		Node temp = head;
+		System.out.println("Node Added : " +temp.getData());
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome Message");
+		//System.out.println("Welcome Message");
+		LinkedListBasic linkedList= new LinkedListBasic();
+		linkedList.addNodeToLinkedList(70);
+		linkedList.display();
 	}
 
 }
